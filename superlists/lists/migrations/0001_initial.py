d2ba2e77020d960eaa8a7a35481aa4c8df1bb8c6
Ahmad Rudi <ahm.rudi@gmail.com>
2014-09-28 12:06:55 +0700

    chapter 06

--- superlists/lists/migrations/0001_initial.py
+++ superlists/lists/migrations/0001_initial.py
@@ -13,11 +13,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
-
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('text', models.TextField(default='')),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(to='lists.List', default=None),
+            preserve_default=True,
         ),
     ]
