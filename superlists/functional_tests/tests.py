67d8bdd6dd9a510efc3b08e71c2a0843c649f5bc
Ahmad Rudi <ahm.rudi@gmail.com>
2014-09-30 22:58:04 +0700

    create web php

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,19 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
-class NewVisitorTest(LiveServerTestCase):
-
+class NewVisitorTest(StaticLiveServerCase):
+	
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url='http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url=cls.live_server_url
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
